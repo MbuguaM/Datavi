05dbeebb9bf6a4aa9ec7f5fada60b2a51242a99b
mbugua <munga2mbugua@gmail.com>
2018-06-01 12:14:25 +0300

    refined the porject structure

--- lyser/models.py
+++ lyser/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+# Create your models here
+class Unit(models.Model):
+    """ model that saves unit data on a project """
+    name = models.CharField(max_length = 20)
+    location =  models.PointField(srid = 4326)
+
+    def __str__(self):
+        return self.name
+        
--- lyser/views.py
+++ lyser/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing(request):
+    """ models displaying the main laning page """
+    return render (request, 'app_temp/landing.html')
