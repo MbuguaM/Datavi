8214f55aaf0a604ff0261cf0738e0a95cc72d92c
mbugua <munga2mbugua@gmail.com>
2018-06-17 16:11:05 +0300

    added layers on the map and template to display documentation info

--- lyser/urls.py
+++ lyser/urls.py
@@ -7,11 +7,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     re_path(r'^$', views.landing, name = 'landing'),
+    re_path(r'^more_about$',views.more_about, name='more_about'),
+    re_path(r'^projection$',views.projection, name='projection'), 
+    re_path(r'^documentation$',views.documentation,name = 'documentation'),
     # loading infomation as json 
     re_path(r'^county_data$',views.county_data, name = 'county_data' ),
     re_path(r'^world_borders$', views.world_borders, name = 'world_borders'),
-    re_path(r'^more_about$',views.more_about, name='more_about'),
-    re_path(r'^projection$',views.projection, name='projection')
 ]
 
 if settings.DEBUG:
--- lyser/views.py
+++ lyser/views.py
@@ -9,7 +9,20 @@ def landing(request):
     """ models displaying the main landing page """
     return render (request, 'app_temp/home.html')
 
+def more_about(request):
+    """ displaying more inforamtion about us """
+    return render(request, 'app_temp/us.html')
 
+def projection(request):
+    """displaying map projection"""
+    return render(request, 'app_temp/projection.html')
+
+def documentation(request):
+    """displaying detailed map infomation"""
+    return render(request, 'app_temp/Documentation.html')
+
+
+# Provider urls ( urls for providing geometric data)
 def county_data(request):
     """ fetching the county data """
     county = serialize('geojson',counties.objects.all())
@@ -19,12 +32,3 @@ def world_borders(request):
     """ fetching the world borders data"""
     world_borders = serialize('geojson',WordBorder.objects.all())
     return HttpResponse(world_borders, content_type = 'json')
-
-
-def more_about(request):
-    """ displaying more inforamtion about us """
-    return render(request, 'app_temp/us.html')
-
-def projection(request):
-    """displaying map projection"""
-    return render(request, 'app_temp/projection.html')
