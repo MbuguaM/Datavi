cddd19e7f8a26c283da869edd2fe0a07bdca8801
ramza007 <ramzalvo007@gmail.com>
2018-06-20 13:57:27 +0300

    minors

--- Datavi/settings.py
+++ Datavi/settings.py
@@ -149,12 +149,12 @@ LEAFLET_CONFIG = {
     'SCALE':'both',
     'ATTRIBUTION_PREFIX':'Mbugua.M@DATAV'
 }
- # email configurations
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+#  # email configurations
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 LOGIN_REDIRECT_URL = ('/')
