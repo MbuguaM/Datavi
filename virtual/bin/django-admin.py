7f99e43b58928b78e1e7dfca82381f16814cb7e6
ramza007 <ramzalvo007@gmail.com>
2018-06-20 19:00:11 +0300

    new virtual folder

--- virtual/bin/django-admin.py
+++ virtual/bin/django-admin.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 #!/home/ramza/Documents/projects/Datavi/virtual/bin/python3.6
-=======
-#!/home/rose/prodev/Datavi/virtual/bin/python
->>>>>>> 4faf08b2f92694d8e9b2418c9d31d56a06a927b5
 from django.core import management
 
 if __name__ == "__main__":
